b937e6cb383efe0c1a2b22f89c4982cc4a9d90a2
Gasmi33 <85202826+Gasmi33@users.noreply.github.com>
2022-04-15 23:35:38 +0000

    na

--- src/main/java/ma/fst/tkhzn/sdsi/business/DemandeService.java
+++ src/main/java/ma/fst/tkhzn/sdsi/business/DemandeService.java
@@ -3,17 +3,20 @@ package ma.fst.tkhzn.sdsi.business;
 import ma.fst.tkhzn.sdsi.entities.*;
 import ma.fst.tkhzn.sdsi.repositories.*;
 import ma.fst.tkhzn.sdsi.requests.DemandeRequest;
+import ma.fst.tkhzn.sdsi.responses.DemandeResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
-import java.util.ArrayList;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping(value = {"api/demandeservice"})
 @CrossOrigin
+
 public class DemandeService {
     @Autowired
     EnseignantRepository enseignantrepository;
@@ -25,30 +28,43 @@ public class DemandeService {
     Ordinateur_dRepository ordinateur_dRepository;
     @Autowired
     UtilisateurRepository utilisateurRepository;
+    @Autowired
+    DemandeRepository demandeRepository;
     @RequestMapping(path = "/addDemande", method = RequestMethod.POST)
     public void addDemande(@RequestBody DemandeRequest demande,Principal login){
         String code = UUID.randomUUID().toString();
         Utilisateur user=utilisateurRepository.findByLogin(login.getName());
         if (demande.getType().equals("Imprimante")){
 
-            Imprimante_d imprimante_d=new Imprimante_d(code,demande.getQteD(),user,0,demande.getImprimante_d().getMarque(),
+            Imprimante_d imprimante_d=new Imprimante_d(code,demande.getQteD(),user,demande.getId_demande(),demande.getImprimante_d().getMarque(),
                     demande.getImprimante_d().getResolution(),demande.getImprimante_d().getVitesse());
             System.out.println(imprimante_d.toString());
             imprimate_dRepository.save(imprimante_d);
 
         }else {
-            Ordinateur_d ordinateur_d=new Ordinateur_d(code,demande.getQteD(),user,0,demande.getOrdinateur_d().getCpu(),demande.getOrdinateur_d().getDisque_d(),
+            Ordinateur_d ordinateur_d=new Ordinateur_d(code,demande.getQteD(),user,demande.getId_demande(),demande.getOrdinateur_d().getCpu(),demande.getOrdinateur_d().getDisque_d(),
                     demande.getOrdinateur_d().getEcran(),demande.getOrdinateur_d().getMarque(),demande.getOrdinateur_d().getRam());
             ordinateur_dRepository.save(ordinateur_d);
         }
     }
     @RequestMapping(path = "/listerDemande", method = RequestMethod.GET)
     public  List<Ressource_d> listerDemande(Principal user){
-        System.out.println(user.getName());
-        for (Ressource_d d:ressource_dRepository.findRess_d(user.getName()))
-            System.out.println(d.toString());
-        //return ressource_dRepository.findRess_d(user.getName());
-        return  null;
+        DemandeResponse demandeResponse =new DemandeResponse();
+        demandeResponse.setRessource_ds(ressource_dRepository.findRess_d(user.getName()));
+        return demandeResponse.getRessource_ds();
+
     }
 
+    @RequestMapping(path = "/saveDemande", method = RequestMethod.POST)
+    public  void  saveDemande(@RequestBody List<DemandeRequest> d, Principal usr) {
+
+        ressource_dRepository.deleteRessource_d();
+        demandeRepository.save(new Demande(utilisateurRepository.findByLogin(usr.getName())));
+        int id = demandeRepository.getId();
+        for (DemandeRequest de : d) {
+            de.setId_demande(id);
+            System.out.println(de);
+            addDemande(de, usr);
+        }
+    }
 }
--- src/main/java/ma/fst/tkhzn/sdsi/repositories/DemandeRepository.java
+++ src/main/java/ma/fst/tkhzn/sdsi/repositories/DemandeRepository.java
@@ -2,6 +2,16 @@ package ma.fst.tkhzn.sdsi.repositories;
 
 import ma.fst.tkhzn.sdsi.entities.Demande;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.Date;
 
 public interface DemandeRepository extends JpaRepository<Demande,Integer> {
+
+
+    @Query("select max(d.id) from Demande d ")
+    public Integer getId();
+
 }
